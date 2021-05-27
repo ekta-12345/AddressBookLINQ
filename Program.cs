8d630de904dc78dd0c764ed399c19fe5f3385a2a
ekta12345 <ektakumbhare3@gmail.com>
2021-05-28 00:48:52 +0530

    Added:Ability to edit contact using their name

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace AddressBookLINQ
             Console.WriteLine("Welcome to the Address Book Data LINQ!");
             // UC 1 Create a new table
             AddressBookRec addressBookTable = new AddressBookRec();
-            addressBookTable.GetAllContacts();
+            //addressBookTable.GetAllContacts();
+            addressBookTable.EditExistingContact("Ekta", "Kumbhare", "PhoneNumber", "9921670015");
+
             Console.ReadLine();
         }
     }
